7f049bdb729d3846bb61e2d23c9e63407565efc6
BorisKiselev <hugh.dancy@icloud.com>
2022-09-22 19:39:15 +0700

    Initial Commit

---
+++ HW16-MVVM+Coordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW16-MVVM+Coordinator
+//
+//  Created by Борис Киселев on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
